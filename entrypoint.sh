429db013f913e8740592d34ac9ac3404f82dc595
illuminatus <trevor.benson@gmail.com>
2020-12-06 11:25:18 -0800

    Update entrypoint.sh
    Fix typo in prometheus port.

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,11 @@ cardano-node --version;
 if [[ $NETWORK = "testnet" ]] ; then
   ln -sf $CNODE_HOME/files/testnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
   ln -sf $CNODE_HOME/files/testnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12789]' $CNODE_HOME/files/testnet-config.json > $CNODE_HOME/files/config.json
+  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/testnet-config.json > $CNODE_HOME/files/config.json
 elif [[ $NETWORK = "mainnet" ]] ; then
   ln -sf $CNODE_HOME/files/mainnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
   ln -sf $CNODE_HOME/files/mainnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12789]' $CNODE_HOME/files/mainnet-config.json > $CNODE_HOME/files/config.json
+  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/mainnet-config.json > $CNODE_HOME/files/config.json
 fi
 
 if ! [[ -z $NETWORK ]] ; then 
