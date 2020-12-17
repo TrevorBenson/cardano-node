ba797d4e1ceead12793c6ed5706bb2c0f6402377
illuminatus <trevor.benson@gmail.com>
2020-12-16 22:44:28 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,25 +14,24 @@ echo "NETWORK: $NETWORK";
 echo "NODE: $HOSTNAME";
 cardano-node --version;
 
-if [[ $NETWORK = "testnet" ]] ; then
-  ln -sf $CNODE_HOME/files/testnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
-  ln -sf $CNODE_HOME/files/testnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-testnet.json > $CNODE_HOME/files/config.json
-elif [[ $NETWORK = "mainnet" ]] ; then
-  ln -sf $CNODE_HOME/files/mainnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
-  ln -sf $CNODE_HOME/files/mainnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-mainnet.json > $CNODE_HOME/files/config.json
-elif [[ $NETWORK = "allegra" ]] ; then
-  ln -sf $CNODE_HOME/files/allegra-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
-  ln -sf $CNODE_HOME/files/allegra-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-allegra.json > $CNODE_HOME/files/config.json
-elif [[ $NETWORK = "launchpad" ]] ; then
-  ln -sf $CNODE_HOME/files/launchpad-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
-  ln -sf $CNODE_HOME/files/launchpad-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-launchpad.json > $CNODE_HOME/files/config.json
-fi
-
 if ! [[ -z $NETWORK ]] ; then 
+  if [[ $NETWORK = "testnet" ]] ; then
+    ln -sf $CNODE_HOME/files/testnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
+    ln -sf $CNODE_HOME/files/testnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
+    jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-testnet.json > $CNODE_HOME/files/config.json
+  elif [[ $NETWORK = "mainnet" ]] ; then
+    ln -sf $CNODE_HOME/files/mainnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
+    ln -sf $CNODE_HOME/files/mainnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
+    jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-mainnet.json > $CNODE_HOME/files/config.json
+  elif [[ $NETWORK = "allegra" ]] ; then
+    ln -sf $CNODE_HOME/files/allegra-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
+    ln -sf $CNODE_HOME/files/allegra-shelley-genesis.json $CNODE_HOME/files/genesis.json
+    jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-allegra.json > $CNODE_HOME/files/config.json
+  elif [[ $NETWORK = "launchpad" ]] ; then
+    ln -sf $CNODE_HOME/files/launchpad-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
+    ln -sf $CNODE_HOME/files/launchpad-shelley-genesis.json $CNODE_HOME/files/genesis.json
+    jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-launchpad.json > $CNODE_HOME/files/config.json
+  fi
   $CNODE_HOME/scripts/cnode.sh
 else
   echo "Please set a NETWORK environment variable to one of: mainnet/testnet/allegra|launchpad"
