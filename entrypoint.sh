3d87a0620e868a370d4285f7cc0d7afff681bfeb
illuminatus <trevor.benson@gmail.com>
2020-12-16 17:06:04 -0800

    Update entrypoint.sh
    Update to:
    1. Add launchpad to entrypoint options
    2. Use original hydra names when creating config.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,17 +17,21 @@ cardano-node --version;
 if [[ $NETWORK = "testnet" ]] ; then
   ln -sf $CNODE_HOME/files/testnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
   ln -sf $CNODE_HOME/files/testnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/testnet-config.json > $CNODE_HOME/files/config.json
+  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-testnet.json > $CNODE_HOME/files/config.json
 elif [[ $NETWORK = "mainnet" ]] ; then
   ln -sf $CNODE_HOME/files/mainnet-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
   ln -sf $CNODE_HOME/files/mainnet-shelley-genesis.json $CNODE_HOME/files/genesis.json
-  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/mainnet-config.json > $CNODE_HOME/files/config.json
+  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-mainnet.json > $CNODE_HOME/files/config.json
+elif [[ $NETWORK = "launchpad" ]] ; then
+  ln -sf $CNODE_HOME/files/allegra-byron-genesis.json $CNODE_HOME/files/byron-genesis.json
+  ln -sf $CNODE_HOME/files/allegra-shelley-genesis.json $CNODE_HOME/files/genesis.json
+  jq '.hasEKG = ["0.0.0.0", 12788] | .hasPrometheus = ["0.0.0.0", 12798]' $CNODE_HOME/files/config-allegra.json > $CNODE_HOME/files/config.json
 fi
 
 if ! [[ -z $NETWORK ]] ; then 
   $CNODE_HOME/scripts/cnode.sh
 else
-  echo "Please set a NETWORK environment variable to one of: testnet/mainnet"
+  echo "Please set a NETWORK environment variable to one of: mainnet/testnet/launchpad"
   echo "To modify topology.json map a container volume of this file to $CNODE_HOME/files/topology.json for a read/write -v /my/files/topology.json:$CNODE_HOME/files/topology.json:rw otherwise set rw to ro if you want it read only inside the container."
-  echo "To enable a POOL pass in POOL_DIR environment variable"
+  echo "To enable a POOL pass in POOL_NAME environment variable"
 fi
